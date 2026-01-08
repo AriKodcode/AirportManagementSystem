58d195435ca290df55914498154a633e7b9d91b5
Ari Durlacher <arikodcode@gmail.com>
2026-01-08 12:50:51 +0200

    added log

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ import { RegularPassenger, StudentPassenger } from './classes/passengers.js';
 
 const benGuryon = new Airport();
 console.log(benGuryon);
-
+console.log(benGuryon.fligths[0]);
+console.log(benGuryon.fligths[1]);
 const student = new StudentPassenger('ari', 212, 1000, 'kodcode');
 const passanger = new RegularPassenger('meir', 1234, 2000, 'kodcode', true);
 
 passanger.buyTicket(benGuryon.fligths[1], 'regular');
 
 student.buyTicket(benGuryon.fligths[0], 'VIP');
+console.log(benGuryon.fligths[0]);
+console.log(benGuryon.fligths[1]);
+console.log(student);
+console.log(passanger);
